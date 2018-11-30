cf0d1bb320def96015bd3bfdef1814ec58729ccd
sengayire <psengayire@gmail.com>
2018-11-30 06:06:12 -0800

    GITBOO

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -10,5 +10,6 @@ usersRouter.get('/', usersController.getAllUsers);
 usersRouter.get('/:id', usersController.getOneUser);
 usersRouter.get('/login', usersController.userSignIn);
 usersRouter.get('/logout', usersController.logout);
+// usersRouter.put('/login', user)
 
 export default usersRouter;
